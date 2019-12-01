9b8048e5ee002f085e88bf0f9dd741305e40c5ef
yichengyang <yicheng.yang@clobotics.com>
2019-12-01 15:03:19 +0800

    separate the project into four modules

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+export const configureWebpack = {
+    resolve: {
+        alias: {
+            'assets': '@/assets',
+            'common': '@/common',
+            'components': '@/components',
+            'network': '@/network',
+        }
+    }
+};
